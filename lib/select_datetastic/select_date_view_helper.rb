dd95b8ebde589b188e1ddf20f222e582ddcd363f
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2012-01-14 07:12:04 -0500

    Tests almost working

--- lib/select_datetastic/select_date_view_helper.rb
+++ lib/select_datetastic/select_date_view_helper.rb
@@ -1,7 +1,13 @@
 module SelectDatetastic
   module SelectDateViewHelper
-    def select_datetastic *args
-      select_date *args
+    def select_datetastic date=nil, options={}, html_options={}, datetastic_options={}
+      select_date_args = [date, options, html_options].compact
+      select_date(*select_date_args).tap do
+        gsub! /date/,  datetastic_options[:date] if datetastic_options[:date]
+        gsub! /year/,  datetastic_options[:date] if datetastic_options[:year]
+        gsub! /month/, datetastic_options[:date] if datetastic_options[:month]
+        gsub! /day/,   datetastic_options[:date] if datetastic_options[:day]
+      end
     end
   end
 end
--- test/test_select_datetastic_view_helper.rb
+++ test/test_select_datetastic_view_helper.rb
@@ -1,11 +1,30 @@
-require "active_support"
-require "rails/railtie"
 require "action_view"
+require "rails/railtie"
 require "test/unit"
 
-require "select_datetastic"
+require "select_datetastic/select_date_view_helper"
 
-class TestSelectDatetasticViewHelper < Test::Unit::TestCase
-  def test_acts_as_wrapper
+class TestSelectDatetasticViewHelper < ActionView::TestCase 
+  
+  def setup
+    ActionView::Base.send :include, SelectDatetastic::SelectDateViewHelper
+    view = ActionView::Base.new
   end
+
+  test "#select_datetastic acts as wrapper for #select_date" do
+    args = [Time.now, prompt: {day: 'Choose day', month: 'Choose month', year: 'Choose year'}]
+    puts view.select_datetastic
+    assert_equal  view.select_date,
+                  view.select_datetastic
+    assert_equal view.select_date(*args), 
+                 view.select_datetastic(*args)
+    puts view.select_date
+  end
+
+  test "#select_datetastic allows for name=hash[key] customization" do
+    args = [Time.now, {prompt: {day: 'Choose day', month: 'Choose month', year: 'Choose year'}}, {class: 'datetastic'}, {date: 'user'}]
+    assert_equal false, (view.select_date *(args[0..-2])).match(/name=user\[/)
+    assert_equal true, (view.select_datetastic *args).match(/name=user\[/)
+  end
+
 end
